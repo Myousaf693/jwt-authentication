68142e588233312393648337176ebec8af55f1c7
M Yousaf Ali Shah <yousafshahyusaf@gmail.com>
2025-05-13 11:36:20 +0500

    Ui Updated

--- frontend/src/pages/auth/logout/Logout.jsx
+++ frontend/src/pages/auth/logout/Logout.jsx
@@ -1,23 +1,4 @@
-// import React, { useEffect } from 'react'
-// import { Navigate } from 'react-router-dom';
-// import { useAuth } from '../../../store/auth';
-
-
-// const Logout = () => {
-//   const { LogoutUser } = useAuth();
-// useEffect(()=>{
-// LogoutUser();
-// },[LogoutUser])
-
-//   return (
-//     <Navigate to={'/login'}/>
-//   )
-// }
-
-// export default Logout
-
-// ////////////////////////////////////
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Navigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../../redux/features/auth/authSlice';
@@ -26,11 +7,16 @@ import toast from 'react-hot-toast';
 
 const Logout = () => {
   const dispatch = useDispatch();
+  const hasLoggedOut = useRef(false);
+
 
 useEffect(()=>{
-dispatch(logout())
+  if(!hasLoggedOut.current){
+    dispatch(logout());
+    toast.success("User Logged out successfully");
+    hasLoggedOut.current = true;
+  }
 },[dispatch])
-toast.success("User Logged out successfully")
 
   return (
     <Navigate to={'/login'}/>
